c200bd5a529dbd77b7e23e9c85e1269d37e89042
shibin kp <shibinkp1012@gmail.com>
2025-04-11 15:02:31 +0530

    The first commit a Structure created

---
+++ Netflix/src/components/TitleCards/TItleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TItleCards = () => {
+  return (
+    <div className='TitleCards'>
+      
+    </div>
+  )
+}
+
+export default TItleCards
---
+++ Netflix/src/pages/Player/Player.jsx
@@ -0,0 +1,12 @@
+import './Player.css'
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className='Player'>
+      
+    </div>
+  )
+}
+
+export default Player
